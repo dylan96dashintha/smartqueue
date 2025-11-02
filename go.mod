77d23dc3a01d78be5c501e08acaad74f1a7deb0b
dylan96dashintha <djdilan09@gmail.com>
2025-11-02 17:15:24 +0530

    go mod and go main file

---
+++ go.mod
@@ -0,0 +1,3 @@
+module dylan96dashintha/smartqueue
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,36 @@
+package queue
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	store := NewTenantStore(1000)
+	defer store.Close()
+
+	callback := func(tenantId string, key int64) {
+		fmt.Printf("key: %d, tenantId: %v , fire the init_cancel event", key, tenantId)
+	}
+	//go func() {
+	store.Enqueue("t0001", 121, "apple", callback, 6*time.Second)
+	//}()
+
+	go func() {
+		store.Enqueue("t0002", 124, "banana", callback, 15*time.Second)
+	}()
+	go func() {
+		store.Enqueue("t0001", 125, "avocado", callback, 15*time.Second)
+
+	}()
+
+	fmt.Println(store.Pop("t0001", 121)) // apple
+	store.Remove("t0001", 121)
+
+	time.Sleep(6 * time.Second)
+	fmt.Println(store.Pop("t0001", 121)) // expired -> nil, false
+	fmt.Println(store.Pop("t0002", 124)) // still valid
+	//fmt.Println(store.PopFirst("t0001")) // removes oldest valid item (a2)
+	time.Sleep(20 * time.Second)
+	fmt.Println(store.GetTenantOrderedMap("t0001"))
+}
